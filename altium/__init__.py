1aaa9a3fe93d1580c9acf1d3ada8cad2cf38a613
Ryan Sturmer <ryansturmer@gmail.com>
2012-12-11 16:25:02 -0500

    Made the config file application relative.

--- altium/__init__.py
+++ altium/__init__.py
@@ -4,19 +4,22 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from session import SqliteSessionInterface
 import util
 
-CONFIG_FILE = os.path.abspath('altium.cfg')
+CONFIG_FILE = 'altium.cfg'
 
 app = Flask(__name__)
+CONFIG_PATH = os.path.join(app.root_path, CONFIG_FILE)
 app.config.from_object('altium.config')
-app.config.from_pyfile(CONFIG_FILE, silent=True)
-util.save_config(app.config, CONFIG_FILE)
+app.config.from_pyfile(CONFIG_PATH, silent=True)
+util.save_config(app.config, CONFIG_PATH)
 
 # Server-side sessions
+
 path = app.config['SESSION_PATH']                   
 if not os.path.exists(path):
     os.mkdir(path)
     os.chmod(path, int('700', 8))
 app.session_interface = SqliteSessionInterface(path)
+
 
 # Initial check of the library to establish SVN data
 library = util.SVNLibrary()
