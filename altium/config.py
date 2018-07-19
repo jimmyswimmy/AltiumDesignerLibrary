04e561b4c57e115e75fb203254d6580f2a664bb4
vagrant <vagrant@localhost.localdomain>
2018-07-19 19:12:55 +0000

    Disable annoying SQLAlchemy track modifications warnings.

--- altium/config.py
+++ altium/config.py
@@ -7,9 +7,11 @@ SECRET_KEY = 'ALTIUM DESIGNER LIBRARY SECRET KEY 379164825'
 SQLALCHEMY_DATABASE_URI = 'postgres://postgres:password@localhost/altium'
 STATIC_ROOT = None
 
-ALTIUM_SVN_URL = 'http://localhost/svn/repos'
+ALTIUM_SVN_URL = 'svn://localhost/database'
 
-ALTIUM_SYM_PATH = '/path/to/SYM'
-ALTIUM_FTPT_PATH = '/path/to/FTPT'
+ALTIUM_SYM_PATH = '/SYM'
+ALTIUM_FTPT_PATH = '/FTPT'
 
 SESSION_PATH = '.sessions'
+
+TRACK_SQLALCHEMY_MODS = False
